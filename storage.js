3106365fb358e7c29278572fbcb29865d93a2590
Cleiton Ricardo Spagnol <101300606+cleitonricardo@users.noreply.github.com>
2024-08-28 20:21:09 -0300

    Cadastro_Medicamento
    Criado código JS para cadastro de medicamentos vindos do HTML

--- storage.js
+++ storage.js
@@ -11,9 +11,48 @@ const storage = {
     },
   
     save(data) {
+      console.log(data)
       this.data = data;
       localStorage.setItem('medicamentos', JSON.stringify(this.data));
     }
   };
 
+  document.getElementById('CadastroMedicamento').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão de recarregar a página
+
+    // Coletando dados do formulário
+    const name = document.getElementById('nomeMedicamento').value;
+    const ingredient = document.getElementById('principioAtivo').value;
+    const laboratory = document.getElementById('laboratorioMedicamento').value;
+    const price = document.getElementById('valorMedicamento').value;
+    const image = document.getElementById('fotoMedicamento').value;
+
+    if (!name || !ingredient || !laboratory|| !price|| !image) {
+      alert('Por favor, preencha todos os campos.');
+      return; // Impede o envio se algum campo estiver vazio
+  }
+    // Criando o objeto medicamento
+    const newMedication = {
+      name: name,
+      ingredient: ingredient,
+      laboratory: laboratory,
+      price: price,
+      image: image
+    };
+
+    // Recuperando a lista de medicamentos do localStorage
+    let medications = storage.sync();
+
+    // Adicionando o novo medicamento à lista
+    medications.push(newMedication);
+
+    // Salvando a lista atualizada no localStorage
+    storage.save(medications)
+    
+    // Opcional: limpar o formulário após o cadastro
+    document.getElementById('CadastroMedicamento').reset();
+
+    // Exibir uma mensagem de sucesso ou realizar outra ação desejada
+    alert('Medicamento cadastrado com sucesso!');
+});
   
